f1250d008de2ce0b04f6e5082724f0b54c95befd
Zarudnev-Alexandr <alexandrzarudnev57@gmail.com>
2024-07-21 23:14:35 +0300

    добавил logreg роут

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 import asyncio
 
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from .routers import user
 from .routers import upgrade
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Или укажите конкретные домены, если нужно
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user.user_route, prefix='/api/users', tags=['Users'])
 app.include_router(upgrade.upgrade_route, prefix='/api/upgrades', tags=['Upgrades'])
