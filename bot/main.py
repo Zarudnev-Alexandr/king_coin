9120897085076e6874530aa860e3fd4fbaa92f0d
Zarudnev-Alexandr <alexandrzarudnev57@gmail.com>
2024-07-24 22:07:27 +0300

    временно рабочая версия бота

--- bot/main.py
+++ bot/main.py
@@ -5,9 +5,12 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, CommandObject
 from aiogram.fsm.state import StatesGroup, State
-from aiogram.types import InlineKeyboardButton
+from aiogram.types import InlineKeyboardButton, Message
 from aiogram.utils.callback_answer import CallbackAnswerMiddleware
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram_dialog import DialogManager, StartMode, Dialog, Window, setup_dialogs
+from aiogram_dialog.widgets.kbd import Group, WebApp
+from aiogram_dialog.widgets.text import Format, Const
 from environs import Env
 
 from commands.quick_commands import check_args
@@ -26,47 +29,47 @@ class StartSG(StatesGroup):
     start = State()
 
 
-@dp.message(CommandStart(
-    deep_link=True,
-    magic=F.args.regexp(re.compile(r'ref_(\d+)'))
-))
-async def command_start_process(message: types.Message,
-                                command: CommandObject):
-    inviter_id = command.args.split("_")[1] if command.args else ''
-    checked_inviter_id = check_args(inviter_id, message.from_user.id)
-    print('👾👾🤖💩', checked_inviter_id)
-
-    builder = InlineKeyboardBuilder()
-    builder.add(
-        InlineKeyboardButton(text=f'Играть {inviter_id}', web_app=types.WebAppInfo(url='https://king-coin.online:444/'))
-    )
-
-    await message.answer(
-        "🌟 Добро пожаловать!",
-        reply_markup=builder.as_markup()
-    )
-
-
-# @dp.message(CommandStart())
-# async def command_start_process(message: Message, dialog_manager: DialogManager):
-#     await dialog_manager.start(state=StartSG.start, mode=StartMode.RESET_STACK)
+# @dp.message(CommandStart(
+#     deep_link=True,
+#     magic=F.args.regexp(re.compile(r'ref_(\d+)'))
+# ))
+# async def command_start_process(message: types.Message,
+#                                 command: CommandObject):
+#     inviter_id = command.args.split("_")[1] if command.args else ''
+#     checked_inviter_id = check_args(inviter_id, message.from_user.id)
+#     print('👾👾🤖💩', checked_inviter_id)
 #
-#
-# start_dialog = Dialog(
-#     Window(
-#         Format('🌟 Добро пожаловать!'),
-#         Group(
-#             WebApp(Const('Играть'), Const('https://king-coin.online:444/'))
-#         ),
-#         state=StartSG.start
+#     builder = InlineKeyboardBuilder()
+#     builder.add(
+#         InlineKeyboardButton(text=f'Играть {inviter_id}', web_app=types.WebAppInfo(url='https://king-coin.online:444/'))
 #     )
-# )
+#
+#     await message.answer(
+#         "🌟 Добро пожаловать!",
+#         reply_markup=builder.as_markup()
+#     )
+
+
+@dp.message(CommandStart())
+async def command_start_process(message: Message, dialog_manager: DialogManager):
+    await dialog_manager.start(state=StartSG.start, mode=StartMode.RESET_STACK)
+
+
+start_dialog = Dialog(
+    Window(
+        Format('🌟 Добро пожаловать!'),
+        Group(
+            WebApp(Const('Играть'), Const('https://king-coin.online:444/'))
+        ),
+        state=StartSG.start
+    )
+)
 
 
 
 
-# dp.include_router(start_dialog)
-# setup_dialogs(dp)
+dp.include_router(start_dialog)
+setup_dialogs(dp)
 
 
 async def on_startup(bot):
