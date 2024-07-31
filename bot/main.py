e724643780aa83c56e1cbbfb7f7d0e3bb34e69be
Zarudnev-Alexandr <alexandrzarudnev57@gmail.com>
2024-07-31 17:57:54 +0300

    добавляем регистрацию через рефералку в боте 2

--- bot/main.py
+++ bot/main.py
@@ -82,7 +82,6 @@ async def command_start_process(message: types.Message,
         reply_markup=builder.as_markup()
     )
 
-
 @dp.message(CommandStart())
 async def command_start_no_referral(message: types.Message):
     # Собираем данные для регистрации без реферала
