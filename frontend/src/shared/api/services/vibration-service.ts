06747cf638e91385f107e87dc36a0e77526dd922
ic2dent <shamyratbabanazarow@gmail.com>
2024-08-08 09:48:17 +0500

    Поправил баг с вибрации

--- frontend/src/shared/api/services/vibration-service.ts
+++ frontend/src/shared/api/services/vibration-service.ts
@@ -1,22 +1,24 @@
 import {useSettingsStore} from "@/shared/pinia/settings-store.ts";
 
 class VibrationService {
-  private static settingsStore = useSettingsStore();
 
   public static light = () => {
-    if (Telegram.WebApp && this.settingsStore.vibrationOn) {
+    const settingsStore = useSettingsStore();
+    if (Telegram.WebApp && settingsStore.vibrationOn) {
       Telegram.WebApp.HapticFeedback.impactOccurred('light');
     }
   }
 
   public static medium = () => {
-    if (Telegram.WebApp && this.settingsStore.vibrationOn) {
+    const settingsStore = useSettingsStore();
+    if (Telegram.WebApp && settingsStore.vibrationOn) {
       Telegram.WebApp.HapticFeedback.impactOccurred('medium');
     }
   }
 
   public static heavy = () => {
-    if (Telegram.WebApp && this.settingsStore.vibrationOn) {
+    const settingsStore = useSettingsStore();
+    if (Telegram.WebApp && settingsStore.vibrationOn) {
       Telegram.WebApp.HapticFeedback.impactOccurred('heavy');
     }
   }
