25c44859bc82acfd26a835196de4deec300c0bc1
ic2dent <shamyratbabanazarow@gmail.com>
2024-08-11 15:23:33 +0500

    Добавил моргание после просмотра игры

--- frontend/src/views/game-view/phaser/entities/player.ts
+++ frontend/src/views/game-view/phaser/entities/player.ts
@@ -7,6 +7,7 @@ import Gameplay from "@/views/game-view/phaser/gameplay.ts";
 export default class Player extends Phaser.GameObjects.Sprite {
   gameStore = useGameStore();
   userStore = useUserStore();
+  private blinkTimer?: Phaser.Time.TimerEvent;
 
   constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene, x, y, 'player');
@@ -19,19 +20,19 @@ export default class Player extends Phaser.GameObjects.Sprite {
       this.body.setGravityY(500);
       this.body.setCollideWorldBounds(true);
 
-      // Устанавливаем круговую форму тела коллизии
       const radius = 350;
       this.body.setCircle(radius, 200, 120);
 
-      // Устанавливаем цвет отладки тела
-      this.body.debugBodyColor = 0xff0000; // Красный цвет
+      this.body.debugBodyColor = 0xff0000;
 
-      // Добавляем прослушивание события 'worldbounds'
       this.body.onWorldBounds = true;
       scene.physics.world.on('worldbounds', this.handleWorldBoundsCollision, this);
     }
 
     scene.input.on('pointerdown', this.handleJump, this);
+
+    // Начинаем моргание, если персонаж неуязвим
+    this.scene.events.on('update', this.update, this);
   }
 
   private handleJump() {
@@ -57,6 +58,48 @@ export default class Player extends Phaser.GameObjects.Sprite {
     (this.body as Phaser.Physics.Arcade.Body).setVelocityY(-300);
   }
 
+  private handleWorldBoundsCollision() {
+    if (this.gameStore.isInvulnerable) return;
+
+    if (this.gameStore.currentActiveModal !== 'game-over') {
+      this.gameStore.audioManager.playGameOverMusic();
+      this.gameStore.vibrationService.heavy();
+    }
+
+    this.gameStore.setPause(true);
+    this.gameStore.setCurrentActiveModal('game-over');
+    this.gameStore.setMysteryBox(null);
+    Gameplay.instance?.clearAllTimeouts();
+  }
+
+  private startBlinking() {
+    if (this.blinkTimer) return;
+
+    this.blinkTimer = this.scene.time.addEvent({
+      delay: 100, // Задержка между переключениями видимости
+      callback: () => {
+        this.visible = !this.visible;
+      },
+      loop: true,
+    });
+  }
+
+  private stopBlinking() {
+    if (this.blinkTimer) {
+      this.blinkTimer.remove();
+      this.blinkTimer = undefined;
+      this.visible = true; // Убедитесь, что спрайт видим после остановки моргания
+    }
+  }
+
+  update() {
+    if (this.gameStore.isInvulnerable) {
+      this.startBlinking();
+    } else {
+      this.stopBlinking();
+    }
+  }
+
   public stopMovement() {
     if (this.body && this.body instanceof Phaser.Physics.Arcade.Body) {
       this.body.setVelocity(0, 0);
@@ -73,19 +116,5 @@ export default class Player extends Phaser.GameObjects.Sprite {
     if (this.body && this.body instanceof Phaser.Physics.Arcade.Body) {
       this.body.setGravityY(600);
     }
-  }
-
-  private handleWorldBoundsCollision() {
-    if (this.gameStore.isInvulnerable) return;
-
-    if (this.gameStore.currentActiveModal !== 'game-over') {
-      this.gameStore.audioManager.playGameOverMusic();
-      this.gameStore.vibrationService.heavy();
-    }
-
-    this.gameStore.setPause(true);
-    this.gameStore.setCurrentActiveModal('game-over');
-    this.gameStore.setMysteryBox(null);
-    Gameplay.instance?.clearAllTimeouts();
   }
 }
