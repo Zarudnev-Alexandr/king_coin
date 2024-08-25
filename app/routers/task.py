ea07611a590a0d9152c4b2e6748a85188097c454
Zarudnev-Alexandr <alexandrzarudnev57@gmail.com>
2024-08-25 20:43:10 +0300

    чиним задания

--- app/routers/task.py
+++ app/routers/task.py
@@ -93,14 +93,14 @@ async def check_task_completion(task_id: int, initData: str = Header(...), db: A
         raise HTTPException(status_code=400, detail="Task timed out")
 
     user_task = await get_user_task(db, task_id, user.tg_id)
-    if not user_task:
-        raise HTTPException(status_code=400, detail="Task not started yet")
-
-    if user_task.completed:
-        raise HTTPException(status_code=400, detail="Task already completed")
 
     # Если это задание без проверки (GENERIC)
     if task.type == TaskType.GENERIC:
+        if not user_task:
+            raise HTTPException(status_code=400, detail="Task not started yet")
+
+        if user_task.completed:
+            raise HTTPException(status_code=400, detail="Task already completed")
         # Проверяем, прошло ли 15 минут с момента создания задания
         time_elapsed = datetime.utcnow() - user_task.created
         if time_elapsed < timedelta(minutes=15):
@@ -113,17 +113,24 @@ async def check_task_completion(task_id: int, initData: str = Header(...), db: A
     elif task.type == TaskType.INVITE:
         invited_count = await get_invited_count(db, user.tg_id)
         if invited_count < task.requirement:
+            if not user_task:
+                await create_user_task(db, task_id, user.tg_id)
             raise HTTPException(status_code=400,
                                 detail="The task was not completed, the number of invited users is not enough")
 
+        if not user_task:
+            user_task = await create_user_task(db, task_id, user.tg_id)
         await complete_user_task(db, user, task, user_task)
 
     elif task.type == TaskType.SUBSCRIBE_TELEGRAM:
         is_subscribed = check_telegram_subscription(task.requirement, user.tg_id)
         if not is_subscribed:
+            if not user_task:
+                await create_user_task(db, task_id, user.tg_id)
             raise HTTPException(status_code=400,
                                 detail="The task was not completed, the user is not subscribed to the channel")
-
+        if not user_task:
+            user_task = await create_user_task(db, task_id, user.tg_id)
         await complete_user_task(db, user, task, user_task)
 
     await db.refresh(user)
