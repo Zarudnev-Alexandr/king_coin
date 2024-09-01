b7ad71e34913a0c4f075fd3d6a35a45ecfa43836
ic2dent <shamyratbabanazarow@gmail.com>
2024-09-01 21:16:02 +0500

    добавлено rollupOptions в build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,24 @@ import vue from '@vitejs/plugin-vue';
 import {resolve} from "path";
 
 export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            "@": resolve(__dirname, "src"),
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Выделяем Vue в отдельный бандл
+          'vue-vendor': ['vue'],
+          // Выделяем другие зависимости в отдельный бандл
+          'vendor': ['vue-router', 'vuex', 'axios'],
         },
-    }
+      },
+    },
+    // Увеличиваем лимит предупреждения для размера чанков, если необходимо
+    chunkSizeWarningLimit: 600,
+  },
 });
