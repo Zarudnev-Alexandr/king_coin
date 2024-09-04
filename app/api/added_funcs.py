b94412f44d802c25efdcfa2a0f8e2881b192f7e0
Zarudnev-Alexandr <alexandrzarudnev57@gmail.com>
2024-09-04 16:53:34 +0300

    добавил вместо сокетов запросы

--- app/api/added_funcs.py
+++ app/api/added_funcs.py
@@ -1,14 +1,18 @@
+import asyncio
 import hashlib
 import hmac
 import json
 import urllib
+from datetime import datetime
 
 from environs import Env
 from fastapi import HTTPException
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.cruds.upgrade import get_user_upgrades, get_upgrade_by_id
 from app.cruds.user import get_user
-
+from app.models import Level
 
 env = Env()
 env.read_env()
@@ -77,5 +81,153 @@ async def decode_init_data(initData: str, db: AsyncSession):
         "user": user if user else None
     }
 
-
     return data_from_init_data
+
+
+async def user_check_and_update(initData: str, db: AsyncSession):
+    referral_rewards = {
+        2: {"no_premium": 15000, "premium": 25000},
+        3: {"no_premium": 35000, "premium": 55000},
+        4: {"no_premium": 40000, "premium": 65000},
+        5: {"no_premium": 65000, "premium": 90000},
+        6: {"no_premium": 95000, "premium": 140000},
+        7: {"no_premium": 200000, "premium": 400000},
+        8: {"no_premium": 500000, "premium": 1000000},
+        9: {"no_premium": 1500000, "premium": 3000000},
+        10: {"no_premium": 3500000, "premium": 6000000},
+    }
+
+    init_data_decode = await decode_init_data(initData, db)
+    user = init_data_decode["user"]
+
+    current_time = datetime.utcnow()
+    last_login = user.last_login or current_time
+    time_diff = current_time - last_login
+
+    hours_passed = min(time_diff.total_seconds() / 3600, 3)
+
+    user_upgrades = await get_user_upgrades(user.tg_id, db)
+    upgrades = await asyncio.gather(
+        *[get_upgrade_by_id(db, user_upgrade.upgrade_id) for user_upgrade in user_upgrades]
+    )
+
+    total_hourly_income = sum(
+        next((lvl.factor for lvl in upgrade.levels if lvl.lvl == user_upgrade.lvl), 0)
+        for user_upgrade, upgrade in zip(user_upgrades, upgrades)
+    )
+
+    total_income = total_hourly_income * hours_passed
+
+    user.money += total_income
+    user.last_login = current_time
+
+    await db.commit()
+    await db.refresh(user)
+
+    levels = await db.execute(select(Level).order_by(Level.lvl))
+    levels = levels.unique().scalars().all()
+
+    levels_list = [level for level in levels if level.lvl > user.lvl]
+    info_about_user_upgrade_lvl = {}
+
+    for level in levels_list:
+        await db.refresh(level)
+        if user.money >= level.required_money and user.lvl < level.lvl:
+            old_lvl = user.lvl
+            user.lvl = level.lvl
+            user.taps_for_level = level.taps_for_level
+            await db.commit()
+            await db.refresh(user)
+
+            # Начисление бонусов пригласившему
+            if user.invited_tg_id:
+                inviter = await get_user(db, user.invited_tg_id)
+                if inviter:
+                    reward = referral_rewards.get(user.lvl, {}).get("premium" if user.is_premium else "no_premium", 0)
+                    inviter.money += reward
+                    await db.commit()
+
+            await db.refresh(user)
+
+            info_about_user_upgrade_lvl = {"event": "new_lvl", "data": {"old_lvl": old_lvl,
+                                                                        "new_lvl": user.lvl,
+                                                                        "new_taps_for_lvl": user.taps_for_level}}
+    return {"money": user.money,
+            "total_hourly_income": total_hourly_income,
+            "total_income": total_income,
+            "hours_passed": hours_passed,
+            "info": info_about_user_upgrade_lvl if info_about_user_upgrade_lvl else None
+            }
+
+
+async def user_check_and_update_without_init_data(user, db: AsyncSession):
+    referral_rewards = {
+        2: {"no_premium": 15000, "premium": 25000},
+        3: {"no_premium": 35000, "premium": 55000},
+        4: {"no_premium": 40000, "premium": 65000},
+        5: {"no_premium": 65000, "premium": 90000},
+        6: {"no_premium": 95000, "premium": 140000},
+        7: {"no_premium": 200000, "premium": 400000},
+        8: {"no_premium": 500000, "premium": 1000000},
+        9: {"no_premium": 1500000, "premium": 3000000},
+        10: {"no_premium": 3500000, "premium": 6000000},
+    }
+
+    current_time = datetime.utcnow()
+    last_login = user.last_login or current_time
+    time_diff = current_time - last_login
+
+    hours_passed = min(time_diff.total_seconds() / 3600, 3)
+
+    user_upgrades = await get_user_upgrades(user.tg_id, db)
+    upgrades = await asyncio.gather(
+        *[get_upgrade_by_id(db, user_upgrade.upgrade_id) for user_upgrade in user_upgrades]
+    )
+
+    total_hourly_income = sum(
+        next((lvl.factor for lvl in upgrade.levels if lvl.lvl == user_upgrade.lvl), 0)
+        for user_upgrade, upgrade in zip(user_upgrades, upgrades)
+    )
+
+    total_income = total_hourly_income * hours_passed
+
+    user.money += total_income
+    user.last_login = current_time
+
+    await db.commit()
+    await db.refresh(user)
+
+    levels = await db.execute(select(Level).order_by(Level.lvl))
+    levels = levels.unique().scalars().all()
+
+    levels_list = [level for level in levels if level.lvl > user.lvl]
+    info_about_user_upgrade_lvl = {}
+
+    for level in levels_list:
+        await db.refresh(level)
+        if user.money >= level.required_money and user.lvl < level.lvl:
+            old_lvl = user.lvl
+            user.lvl = level.lvl
+            user.taps_for_level = level.taps_for_level
+            await db.commit()
+            await db.refresh(user)
+
+            # Начисление бонусов пригласившему
+            if user.invited_tg_id:
+                inviter = await get_user(db, user.invited_tg_id)
+                if inviter:
+                    reward = referral_rewards.get(user.lvl, {}).get("premium" if user.is_premium else "no_premium", 0)
+                    inviter.money += reward
+                    await db.commit()
+
+            await db.refresh(user)
+
+            info_about_user_upgrade_lvl = {"event": "new_lvl", "data": {"old_lvl": old_lvl,
+                                                                        "new_lvl": user.lvl,
+                                                                        "new_taps_for_lvl": user.taps_for_level}}
+    return {"money": user.money,
+            "total_hourly_income": total_hourly_income,
+            "total_income": total_income,
+            "hours_passed": hours_passed,
+            "info": info_about_user_upgrade_lvl if info_about_user_upgrade_lvl else None
+            }
